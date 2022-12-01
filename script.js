b2cd34b76b31e083bb4411d10378cb9110af24ad
verbascun <verbascun@protonmail.com>
2022-12-01 22:58:13 +0100

    with class added to make it more scalable

--- script.js
+++ script.js
@@ -1,93 +1,144 @@
+// import {resistorList, showResults} from './findResistors.js';
+
+console.log("START OF SCRIPT")
+
 let e12Series = [1, 1.2, 1.5, 1.8, 2.2, 2.7, 3.3, 3.9, 4.7, 5.6, 6.8, 8.2]
 let e24Series = [1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9, 4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1]
+
+let equationList= ["1/(1/R1 + 1/R2)","R1+R2","1/(1/R1 + 1/R2)","1/(1/R1 + 1/R2)"];
+
+let length = e12Series.length
+
+for (let i = 0; i <= length*6-1; i++) { 
+    e12Series[12+i] = e12Series[i]*10      
+   }
+
 length = e12Series.length
 
+console.log(e12Series)
 
 
-function findResistorValues()
-{
-   var selectedSeries = document.querySelector("input[name='series']:checked").value;
+// showResults();
 
-  console.log(selectedSeries)
-
-let targetResistance  = document.getElementById("targetResistance").value;
-let maxError = document.getElementById("maxError").value;
-
-    for (let i = 0; i <= length*6-1; i++) { 
-        e12Series[12+i] = e12Series[i]*10      
-       }
-
-let results = []
-       
-for (let resistor1 of e12Series)
-   {
-    for (let resistor2 of e12Series)
-    {
-        
-        let resistorResult = 1/(1/resistor1 + 1/resistor2)
-        let difference = (resistorResult-targetResistance)
-        let error = difference/targetResistance * 100
-        // console.log(resistor1,resistor2,resistorResult.toFixed(2),targetResistance,error.toFixed(2))
+console.log("MIDDLE OF SCRIPT")
 
 
-        // console.log(resistorResult)
+// let equation1 = "1/(1/R1 + 1/R2)";
+// let list1 = new resistorList(equation1,targetResistance,e12Series)
 
-        if (Math.abs(error) < maxError && !results.some(calculatedResistor => calculatedResistor.Result === resistorResult.toFixed(2)))
-        {   
-        let calculatedResistor = {
-            "Resistor 1" : resistor1,
-            "Resistor 2" : resistor2,
-            "Result" : resistorResult.toFixed(2),
-            "Error": error.toFixed(2)
-        } 
-        
-        results.push(calculatedResistor)     
+// list1.findResistors()
+    
+// var selectedSeries = document.querySelector("input[name='series']:checked").value;
+
+// console.log(selectedSeries)
+
+
+
+
+
+
+
+console.log("END OF SCRIPT")  
+
+class resistorList{
+    results = [];
+    
+    constructor(equation,targetResistance,resistorSeries){
+        this.equation = equation;
+        this.targetResistance = targetResistance;
+        this.resistorSeries = resistorSeries;
+    }
+   
+    
+
+    findResistors(){
+     let c = 0;
+        // console.log(this.equation)
+        for (let i = 0; i <= length-1; i++)
+        {
+            let R1= e12Series[i];
+           for (let j = c; j <= length-1; j++)
+            {
+                let R2=e12Series[j]
+                let resistorResult = eval(this.equation);
+                let difference = (resistorResult-this.targetResistance);
+                let error = difference/this.targetResistance * 100;
+                // console.log(R1,R2,this.targetResistance, resistorResult.toFixed(2),error.toFixed(2))
+                    let calculatedResistor = {
+                        "Error": error.toFixed(2),
+                        "Resistor 1" : R1,
+                        "Resistor 2" : R2,
+                        "Result" : resistorResult.toFixed(2),
+                    } 
+                this.results.push(calculatedResistor);
+                }
+                c++;
+                // console.log(c)
+                }
+            this.results.sort(function(a, b){return Math.abs(a.Error) - Math.abs(b.Error)}); 
+            // console.log(this.results)
+            this.results = this.results.slice(0,10);
+            // console.log(this.results);
+            return this.results;
         }
-           
-       } 
-      
-   }
-//    console.log(results)
-    results.sort(function(a, b){return Math.abs(a.Error) - Math.abs(b.Error)}); 
-   console.log(results)
+        addtable()
+        {   
+            let divTable = document.getElementById("tables");
 
-   return results
+            console.log (divTable)
+            let table = document.createElement('table');
+            table.id=this.equation
+
+            divTable.append(table)
+            console.log(this.results)
+
+            generateTableHead(table, Object.keys(this.results[0]))
+            generateTable (table, this.results)
 
 
-}
-  
-function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-      let th = document.createElement("th");
-      let text = document.createTextNode(key);
-      th.appendChild(text);
-      row.appendChild(th);
+
+            function generateTableHead(table, data) {
+                let thead = table.createTHead();
+                let row = thead.insertRow();
+                for (let key of data) {
+                  let th = document.createElement("th");
+                  let text = document.createTextNode(key);
+                  th.appendChild(text);
+                  row.appendChild(th);
+                }
+              }
+
+              function generateTable(table, data) {
+                for (let element of data) {
+                  let row = table.insertRow();
+                  for (let key in element) {
+                    let cell = row.insertCell();
+                    let text = document.createTextNode(element[key]);
+                    cell.appendChild(text);
+                  }
+                }
+              }
+        }
+        }
+    
+
+ function showResults(){
+
+            let divTable = document.getElementById("tables");
+
+            divTable.innerHTML = "";
+
+
+        let targetResistance  = document.getElementById("targetResistance").value;
+
+        console.log("showing results");
+
+        for (let eq of equationList){
+            let list1 = new resistorList(eq,targetResistance,e12Series)
+            console.log(eq)
+            list1.findResistors()
+            list1.addtable(eq)
+        }
+    
+    
     }
-  }
-  
-  function generateTable(table, data) {
-    for (let element of data) {
-      let row = table.insertRow();
-      for (key in element) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-      }
-    }
-  }
-  
-  function showResults()
-  {
-    let table = document.querySelector("table");
-    table.innerHTML = "";
-    let resistorValues = findResistorValues()
-    let data = Object.keys(resistorValues[0]);
-    generateTableHead(table, data);
-    generateTable(table, resistorValues);
-
-  }
-
-
-console.log("END")
